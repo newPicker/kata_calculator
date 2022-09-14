84037e56869be54cd76e7af2fdd3126b7d754628
Tatyana <tn.puzanova@mpgu.su>
2022-09-14 23:54:54 +0300

    Реализовали метод для получения строки от пользователя

--- .idea/src/ru.newpicker.kata/Main.java
+++ .idea/src/ru.newpicker.kata/Main.java
@@ -1,7 +1,23 @@
 package ru.newpicker.kata;
 
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
+        String input = getString();
 
     }
+
+
+    private static String getString() {
+        // Получаем выраженние, которое нужно посчитать
+
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("Введите выражение, которое нужно посчитать: ");
+
+        return scanner.nextLine();
+    }
+
+
 }
